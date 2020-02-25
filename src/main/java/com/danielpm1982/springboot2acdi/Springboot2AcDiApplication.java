8b0fa4bca1dcaa9205d45b6862ce3a052aa14e5e
danielpm1982 <danielpm1982@gmail.com>
2020-02-25 18:04:00 -0300

    Add basic Spring Boot 2 project structure

---
+++ src/main/java/com/danielpm1982/springboot2acdi/Springboot2AcDiApplication.java
@@ -0,0 +1,11 @@
+package com.danielpm1982.springboot2acdi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2AcDiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2AcDiApplication.class, args);
+	}
+
+}
